07662ff0314a77800be5e9e9e9b7dfa0516e2ec5
Marriane Decierra <decierramarriane12@gmail.com>
2024-03-17 15:36:12 +0800

    bookstore

---
+++ about.php
@@ -0,0 +1,231 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- css file link  -->
+   <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/booky.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Our books publish with a vision in mind and mission at heart, and we’re happy to help them to sell a book that can accomplish both. From start to finish, you’ll receive world class care. Our employees fall in love with the books, and that means they truly care about the end product.</p>
+         <p>"Some books leave us free and some books make us free." – Ralph Waldo Emerson</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic1.jpg" alt="">
+         <p>Well - packaged with bubble wrap, this is the best online bookstore.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>John</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic2.jpg" alt="">
+         <p>My book order was packed carefully and received it in good condition! </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Ruiz</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic3.jpg" alt="">
+         <p>Fast shipping within 3 days, good condition also and no dents</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Xiao</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic4.jpg" alt="">
+         <p>Affordable books, good packaging!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Anya</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic5.jpg" alt="">
+         <p>Best bookshop ever, this is the first time to buy.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Kanna</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic6.jpg" alt="">
+         <p>Affordable original books!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Umaru</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">great authors</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/aut1.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>James Clear</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/aut2.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Mark Manson</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/aut3.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>J.K. Rowling</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/aut4.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Taylor Jenkins Reid</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/aut5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Harper Lee</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/aut6.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Suzanne Collin</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!--  js file link  -->
+<script src="script.js"></script>
+
+</body>
+</html>
